5611dc9a70be6d29e1203c3eb03c43a391aa1506
cyrll29 <christiancyrilleyvillasenor@iskolarngbayan.pup.edu.ph>
2023-02-07 23:16:34 +0800

    landing page update

---
+++ JS/index.js
@@ -0,0 +1,41 @@
+let activeIndex = 0;
+
+const groups = document.getElementsByClassName("card-group");
+
+const handleRightClick = () => {
+    // Bump active index
+
+    const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+
+    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`), 
+    nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+    // Active group becomes after
+
+    currentGroup.dataset.status = "after";
+
+    // Next group becomes active
+
+    nextGroup.dataset.status = "becoming-active-from-before";
+
+    setTimeout(() => {
+        nextGroup.dataset.status = "active";
+        activeIndex = nextIndex;
+    });
+}
+
+const handleLeftClick = () => {
+    const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+
+    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`), 
+    nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+    currentGroup.dataset.status = "before";
+
+    nextGroup.dataset.status = "becoming-active-from-after"
+
+    setTimeout(() => {
+        nextGroup.dataset.status = "active";
+        activeIndex = nextIndex;
+    });
+} 
